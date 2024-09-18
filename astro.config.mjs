ec1523fa6bf825bba59ed1a0bc87fdc7063c945a
lotap <code@lotap.dev>
2024-09-18 15:06:48 -0600

    real init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "wunshot",
+      social: {
+        github: "https://github.com/lotap/wunshot",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            { label: "Introduction", slug: "index" },
+            { label: "Prerequisites", slug: "getting-started/prerequisites" },
+            { label: "Installation", slug: "getting-started/installation" },
+            {
+              label: "Base Schema & User Operations",
+              slug: "getting-started/base-schema-and-user-operations",
+            },
+          ],
+        },
+        {
+          label: "Auth",
+          badge: "Soon",
+          items: [
+            // { label: "Sessions", slug: "auth/sessions" },
+            // { label: "Username & Password", slug: "auth/username-password" },
+            // { label: "Magic Code", slug: "auth/magic-code" },
+            // { label: "OAuth", slug: "auth/oauth" },
+          ],
+        },
+      ],
+      expressiveCode: {
+        themes: ["catppuccin-latte", "catppuccin-macchiato"],
+      },
+      customCss: [
+        "@fontsource-variable/mulish/wght.css",
+        "@fontsource-variable/rubik/wght.css",
+        "@fontsource-variable/jetbrains-mono/wght.css",
+        "./src/styles/tailwind.css",
+        "./src/styles/custom.css",
+      ],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,26 @@
-import colors from 'tailwindcss/colors';
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import starlightPlugin from "@astrojs/starlight-tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
-		},
-	},
-	plugins: [starlightPlugin()],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Rubik Variable", ...defaultTheme.fontFamily.sans],
+      mono: ["JetBrains Mono Variable", ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
+        accent: colors.violet,
+        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+        gray: colors.zinc,
+      },
+      fontFamily: {
+        display: ["Mulish Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
 };
