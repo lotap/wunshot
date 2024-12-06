60e64220012e7c94c872c6abc64c052d213fc822
lotap <6519009+lotap@users.noreply.github.com>
2024-12-06 11:30:49 -0700

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lotap.github.io/",
-  base: "/wunshot/",
+  // site: "https://lotap.github.io/",
+  // base: "/wunshot/",
+  site: "https://www.wunshot.io/",
   integrations: [
     starlight({
       title: "wunshot",
