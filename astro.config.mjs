9fa704ac05e3708e06a9472219d4fabba198f205
lotap <6519009+lotap@users.noreply.github.com>
2024-12-06 11:30:49 -0700

    update docs url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lotap.github.io/wunshot/",
+  base: "/wunshot/",
   integrations: [
     starlight({
       title: "wunshot",
