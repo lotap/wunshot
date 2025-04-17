c6d8aaea3c5be04818febc7506e39fe3211d382b
lotap <6519009+lotap@users.noreply.github.com>
2025-04-17 01:45:11 -0600

    Fundamentals of Auth section

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,6 +46,7 @@ export default defineConfig({
           },
           items: [
             { label: "Overview", slug: "auth/overview" },
+            { label: "Universal Ops", slug: "auth/universal-ops" },
             { label: "Storage Approaches", slug: "auth/storage-approaches" },
             // { label: "Sessions", slug: "auth/sessions" },
             { label: "Username & Password", slug: "auth/username-password" },
